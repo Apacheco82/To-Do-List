fc33e134ae19d8dec8a2bf6589c6484dcee72cf2
Ana Pacheco <kornetera@gmail.com>
2023-02-11 12:13:06 +0000

    changes

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-window.onload = () => {
-  document.querySelector("#button").addEventListener("click",() => {
-    document.querySelector("#excuse").innerHTML = generadorExcusa()
-  })
-}
 
-
-function generadorExcusa() {
-  let who = [
-    "Mi gato",
-    "Su abuela",
-    "El loro del vecino",
-    "Pikachu",
-    "El repartidor de Amazon",
-    "Tu prima la del pueblo",
-    "El profesor"
-  ];
-  let what = ["se lo comió", "lo rompió", "explotó", "escupió"];
-  let when = [
-    "después de clase",
-    "justo a tiempo",
-    "cuando había terminado",
-    "mientras comía",
-    "cuando me estaba duchando",
-    "cuando iba a finalizar"
-  ];
-  let primer = Math.floor(Math.random() * who.length);
-  let segundo = Math.floor(Math.random() * what.length);
-  let tercer = Math.floor(Math.random() * when.length);
+let todo = ""
 
 
 
-  let output = `${who[primer]} ${what[segundo]} ${when[tercer]}`
+const formToDo = document.querySelector("#formTodo")
 
- return output
-};
+console.log(formToDo)
+
+
+const toDoList = [{
+  title: "",
+  descripcion : "",
+  isReady: false,
+  date : new Date()
+}]
+
+formToDo.addEventListener("change", (event) => {
+  console.log(event.target.value)
+
+})
+
+
+
+
+
